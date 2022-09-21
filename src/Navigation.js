880bffd1c2caae4d297c3305cef18c724d0de6d5
Sem Thijssen <30442@ma-web.nl>
2022-09-21 15:33:47 +0200

    navigation erin gezet

---
+++ src/Navigation.js
@@ -0,0 +1,9 @@
+import "./Navigation.css"
+const Navigation = () =>{
+ return( 
+ <nav class = "navigation">
+    <figure class="navigation__logo"> <i class="fa-solid fa-mug-hot"></i></figure>
+</nav>
+ );
+}
+export default Navigation;
