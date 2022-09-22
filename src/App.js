53103cb5eb35496c0de0e2574ff6aaf5572703ad
Sem Thijssen <30442@ma-web.nl>
2022-09-22 13:07:53 +0200

    achtergrond gestyled en font en header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Navigation from "./Navigation";
+import Header from "./Header";
 import"./App.css";
 const App = () => {
     return(
-    <Navigation/>
+    <><Navigation/>
+        <Header bigTitle="coffeez" smallTitle="Coffee that will make you smile." />
+        </>
     );
+    
 }
 export default App;
